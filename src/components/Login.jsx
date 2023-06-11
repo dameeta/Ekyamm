c15888b22d2916c69f6128f40d1ad3b59aaffaa4
Gunjan Shukla <86796240+GunjanShukla1422@users.noreply.github.com>
2023-06-12 00:00:28 +0530

    Login page UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,35 @@
-import { Box, Button, TextField } from '@mui/material'
+import { Box, Button, Container, FormControl, TextField } from '@mui/material'
 import React from 'react'
 import "../assets/styles/Login.css"
+import MaxHealthCare from "../assets/logo/MaxHealthCare.png"
+import Ekyamm from "../assets/logo/Ekyamm.png"
 
 const Login = () => {
   return (
-    <Box className='login-Main'>
+    <Box width="100%" className='login-Main'>
+      <Container>
         <div className='loginForm'>
-          <div>   
-              <div className="loginInput">
-                <TextField
-                  // className='loginInput'
-                  id="outlined-error-helper-text"
-                  label="Email"
-                />
-              </div>
-
-              <div className="loginInput">
-                <TextField
-                  // className='loginInput'
-                  id="outlined-error-helper-text"
-                  label="Password"
-                />
-              </div>
-
-            <div>
-              <Button className='loginButton' variant="contained">Login</Button>
+          <FormControl>  
+            <div className='loginHeader'>
+              <img src={MaxHealthCare} height="50px" width="100px" alt="Logo" />
             </div>
-          </div>   
+            <div className='loginHeader'>
+              <h4>Login to continue</h4>
+            </div>
+            <input className="loginInput" placeholder='Enter email address'/>  
+            <input className="loginInput" placeholder='Enter password'/>  
+              
+          </FormControl>   
+          <div className='loginSubmitButton'>
+            <p>Forgot Password?</p>
+            <Button className='loginButton' variant="contained">Login</Button>
+          </div>
         </div>
+        <div className='footer'>
+          <p>Copyright © 2023</p>
+          <img src={Ekyamm} alt="Logo" />
+        </div>
+      </Container>
     </Box>
   )
 }
